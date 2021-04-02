9b9d98ee30b07fd05801bfef8746990fafcd53a9
AlishaGaha <gahaalisha13@gmail.com>
2021-04-02 13:43:12 +0545

    gmail task scheduling

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -37,31 +37,31 @@ class Kernel extends ConsoleKernel
         if(isset($clients)) {
             foreach( $clients as $client) {
                 if($client->domain_renewal_type == 'Month' || $client->hosting_renewal_type == 'Month') {
-                    $expiry_date = isset($client->domain_renewal) ?
-                        $client->updated_at->format('Y-m-d')
-                        :
-                        $client->updated_at->format('Y-m-d');
                     // $expiry_date = isset($client->domain_renewal) ?
-                    //     $client->updated_at->addMonths($client->domain_renewal)->subDays(5)->format('Y-m-d')
+                    //     $client->updated_at->format('Y-m-d')
                     //     :
-                    //     $client->updated_at->addMonths($client->hosting_renewal)->subDays(5)->format('Y-m-d');
+                    //     $client->updated_at->format('Y-m-d');
+                    $expiry_date = isset($client->domain_renewal) ?
+                        $client->updated_at->addMonths($client->domain_renewal)->subDays(5)->format('Y-m-d')
+                        :
+                        $client->updated_at->addMonths($client->hosting_renewal)->subDays(5)->format('Y-m-d');
                     if($currentDate == $expiry_date) {
-                        // $schedule->job(new SendEmailJob($client->email, $client->first_name))->dailyAt('9:00');
-                        $schedule->job(new SendEmailJob($client->email, $client->first_name))->everyMinute();
+                        $schedule->job(new SendEmailJob($client->email, $client->first_name))->dailyAt('9:00');
+                        // $schedule->job(new SendEmailJob($client->email, $client->first_name))->everyMinute();
                     }
                     // $schedule->job(new SendEmailJob($client->email))->monthlyOn($client->updated_at->format('d'), '9:00');
                 } else if($client->domain_renewal_type == 'Year' || $client->hosting_renewal_type == 'Year') {
-                    $expiry_date = isset($client->domain_renewal) ?
-                        $client->updated_at->format('Y-m-d')
-                        :
-                        $client->updated_at->format('Y-m-d');
                     // $expiry_date = isset($client->domain_renewal) ?
-                    //     $client->updated_at->addYears($client->domain_renewal)->subMonths(1)->format('Y-m-d')
+                    //     $client->updated_at->format('Y-m-d')
                     //     :
-                    //     $client->updated_at->addYears($client->hosting_renewal)->subMonths(1)->format('Y-m-d');
+                    //     $client->updated_at->format('Y-m-d');
+                    $expiry_date = isset($client->domain_renewal) ?
+                        $client->updated_at->addYears($client->domain_renewal)->subMonths(1)->format('Y-m-d')
+                        :
+                        $client->updated_at->addYears($client->hosting_renewal)->subMonths(1)->format('Y-m-d');
                     if($currentDate == $expiry_date) {
-                        // $schedule->job(new SendEmailJob($client->email, $client->first_name))->dailyAt('9:00');
-                        $schedule->job(new SendEmailJob($client->email, $client->first_name))->everyMinute();
+                        $schedule->job(new SendEmailJob($client->email, $client->first_name))->dailyAt('9:00');
+                        // $schedule->job(new SendEmailJob($client->email, $client->first_name))->everyMinute();
                     }
                 }
             }
